c6df5ddad325363e85ac457910c41b7a611fc811
Stefan Mueller <s.mueller@it.kls-glt.de>
2014-06-13 10:47:55 +0200

    Initialer commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Grafik Bibliothek
+## hauptsächlich für EA DOGM128 an PIC
+
+
+Diese Bibliothek ist abgeleitet von:
+-  http://edeca.net/wp/electronics/the-st7565-display-controller/
+
+
+# Weitere Links
+- (Erklärung des Algorithmus der zur Zeichnung verwendet wird)[http://www.gamedev.net/page/resources/_/technical/game-programming/line-drawing-algorithm-explained-r1275]
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Graphic.null
+CND_ARTIFACT_PATH_default=dist/default/production/Graphic.null
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=Graphic.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/Graphic.tar
